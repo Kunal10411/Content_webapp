2f23a569286967693f69f3678c92352964fb0e16
Kunal <Kunaldas1423@gmail.com>
2024-11-27 12:04:45 +0530

    updated

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-import os
-from app import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    import os
+    port = int(os.getenv("PORT", 5000))  # Use Render's PORT env variable
     app.run(host='0.0.0.0', port=port)
